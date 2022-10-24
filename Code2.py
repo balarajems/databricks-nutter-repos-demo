5680a14270efb42065487ed35bee898fa03fe1ad
Babatunde Alaraje <balaraje@microsoft.com>
2022-10-24 10:41:09 -0500

    add sample code

--- Code2.py
+++ Code2.py
@@ -7,10 +7,23 @@ def generate_data2(table_name="my_data"):
   df.write.format("delta").mode("overwrite").saveAsTable(table_name)
 
 
-def read_covid19_data(table_name="covid19"):
-    spark = SparkSession.builder.appName('Covid19 Metrics').getOrCreate()
+def read_covid19_data(df):
+    # covid19_df.printSchema()
+    rows_data = df.collect()
+    deaths_by_country = {}
+    
+    for row in rows_data:
+        country = row.Country_Region
+        if country not in deaths_by_country:
+            deaths_by_country[country] = 0
+        deaths_by_country[country] += int(row.Deaths)
+        
+    return deaths_by_country
 
-    df = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/shared_uploads/balaraje@microsoft.com/covid19.csv")
-    df.show()
-    df.head(3)
-    df.printSchema()
+# Calling the read_covid19_data function
+
+# spark = SparkSession.builder.appName('Covid19 Metrics').getOrCreate()
+# covid19_df = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/tables/covid19.csv")
+
+# result = read_covid19_data(covid19_df)
+# print(result)
