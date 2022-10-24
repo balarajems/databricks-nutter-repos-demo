a9f2a026acd7110443989b6ec2d8f1416ad91226
Babatunde Alaraje <balaraje@microsoft.com>
2022-10-24 01:03:59 -0500

    add new test

--- my_package/code2.py
+++ my_package/code2.py
@@ -3,4 +3,19 @@ from pyspark.sql import SparkSession
 def generate_data2(table_name="my_data"):
   df = SparkSession.getActiveSession().range(0,10)
   df.write.format("delta").mode("overwrite").saveAsTable(table_name)
-  
+
+
+def read_covid19_data(table_name="covid19"):
+    spark = SparkSession.builder.appName('Covid19 Metrics').getOrCreate()
+    covid19_df = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/tables/covid19.csv")
+    covid19_df.printSchema()
+    rows_data = covid19_df.collect()
+    deaths_by_country = {}
+    
+    for row in rows_data:
+        country = row.Country_Region
+        if country not in deaths_by_country:
+            deaths_by_country[country] = 0
+        deaths_by_country[country] += int(row.Deaths)
+        
+    return deaths_by_country
