3903b728a79070f753e7685f504f8f020707cda9
Babatunde Alaraje (C) <balaraje@microsoft.com>
2023-01-05 13:10:26 -0600

    update notebook

--- Metrics.py
+++ Metrics.py
@@ -1,10 +1,29 @@
+# Databricks notebook source
 from pyspark.sql import SparkSession
 
 
-def read_covid19_data(table_name="covid19"):
-    spark = SparkSession.builder.appName('Covid19 Metrics').getOrCreate()
+def generate_data2(table_name="my_data"):
+  df = SparkSession.getActiveSession().range(0,10)
+  df.write.format("delta").mode("overwrite").saveAsTable(table_name)
 
-    df = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/shared_uploads/balaraje@microsoft.com/covid19.csv")
-    df.show()
-    df.head(3)
-    df.printSchema()
+
+def read_covid19_data(df):
+    # covid19_df.printSchema()
+    rows_data = df.collect()
+    deaths_by_country = {}
+    
+    for row in rows_data:
+        country = row.Country_Region
+        if country not in deaths_by_country:
+            deaths_by_country[country] = 0
+        deaths_by_country[country] += int(row.Deaths)
+        
+    return deaths_by_country
+
+# Calling the read_covid19_data function
+
+# spark = SparkSession.builder.appName('Covid19 Metrics').getOrCreate()
+# covid19_df = spark.read.format("csv").option("header", "true").load("dbfs:/FileStore/tables/covid19.csv")
+
+# result = read_covid19_data(covid19_df)
+# print(result)
